a1946e0daa86259398f5e22cb561e291745e8e02
Sander0542 <sanderjochems@hotmail.nl>
2019-01-24 15:29:08 +0100

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -1,9 +1,15 @@
 <?php
 
+require 'classes/Cloudflare.php';
+require 'classes/PleskDNS.php';
+
 class IndexController extends pm_Controller_Action
 {
-    public function indexAction()
-    {
+  /**
+   * @var $cloudflare Cloudflare
+   */
+  private $cloudflare;
+
   public function init()
   {
     parent::init();
@@ -33,7 +39,58 @@ class IndexController extends pm_Controller_Action
 
   public function domainsAction()
   {
+    $list = $this->_getDomainList();
 
+    $this->view->list = $list;
   }
+
+  public function domainDataAction()
+  {
+    $list = $this->_getDomainList();
+    // Json data from pm_View_List_Simple
+    $this->_helper->json($list->fetchData());
+  }
+
+  private function _getDomainList()
+  {
+    $zones = $this->cloudflare->getZones();
+
+    $data = array();
+    /**
+     * @var $domain pm_Domain
+     */
+    foreach (pm_Session::getCurrentDomains(true) as $domain) {
+      $cloudflareID = "Zone ID not found";
+      foreach ($zones->listZones()->result as $zone) {
+        if ($zone->name == $domain->getName()) {
+          $cloudflareID = $zone->id;
+          break;
+        }
+      }
+      $data[] = array(
+          'col-id' => count((new PleskDNS())->getRecords($domain->getId())),
+          'col-domain' => $domain->getName(),
+          'col-zone' => $cloudflareID,
+      );
     }
+
+    $list = new pm_View_List_Simple($this->view, $this->_request);
+    $list->setData($data);
+    $list->setColumns(array(
+        'col-id' => array(
+            'title' => 'ID',
+            'noEscape' => true,
+        ),
+        'col-domain' => array(
+            'title' => 'Domain',
+            'noEscape' => true,
+        ),
+        'col-zone' => array(
+            'title' => 'Cloudflare Zone ID',
+            'noEscape' => true,
+        )
+    ));
+    $list->setDataUrl(array('action' => 'domain-data'));
+    return $list;
+  }
 }
