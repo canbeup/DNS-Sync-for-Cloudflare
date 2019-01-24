950d356e9d6f23f993193587afd036b5f53840a7
Sander0542 <sanderjochems@hotmail.nl>
2019-01-24 15:27:25 +0100

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -4,5 +4,22 @@ class IndexController extends pm_Controller_Action
 {
     public function indexAction()
     {
+  public function init()
+  {
+    parent::init();
+
+    // Init title for all actions
+    $this->view->pageTitle = 'Cloudflare DNS Sync';
+  }
+
+  public function indexAction()
+  {
+    $this->forward('domains');
+  }
+
+  public function domainsAction()
+  {
+
+  }
     }
 }
