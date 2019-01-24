e776ca3729b713aa0993db4571c4496b06a69f04
Sander0542 <sanderjochems@hotmail.nl>
2019-01-24 15:28:36 +0100

    Add tabs

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -10,6 +10,20 @@ class IndexController extends pm_Controller_Action
 
     // Init title for all actions
     $this->view->pageTitle = 'Cloudflare DNS Sync';
+
+    // Init tabs for all actions
+    $this->view->tabs = array(
+        array(
+            'title' => 'Domains',
+            'action' => 'domains',
+        ),
+        array(
+            'title' => 'API',
+            'action' => 'api',
+        )
+    );
+
+    $this->cloudflare = new Cloudflare('sanderjochems@hotmail.nl', '384c07aaa0b4b8827155d5de764bda2b3a9c5');
   }
 
   public function indexAction()
