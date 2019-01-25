1ff012137d8140a995dafcf935d7e3eadcc2bb62
Sander Jochems <sanderjochems@hotmail.nl>
2019-01-25 10:45:32 +0100

    Link Domain to Sync Page

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -116,7 +116,7 @@ class IndexController extends pm_Controller_Action
         }
       }
       $data[] = array(
-          'col-domain' => $domain->getName(),
+          'col-domain' => '<a href="'.pm_Context::getBaseUrl().'index.php/index/sync?site_id='.$domain->getId().'">'.$domain->getName().'</a>',
           'col-zone' => $cloudflareID,
       );
     }
