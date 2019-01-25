fdb63c4821ad99e581b34bee3a405e4ba3a913a6
Sander Jochems <sanderjochems@hotmail.nl>
2019-01-26 00:20:33 +0100

    Create CloudflareRecord.php

---
+++ plib/controllers/helpers/CloudflareRecord.php
@@ -0,0 +1,18 @@
+<?php
+
+class CloudflareRecord
+{
+  public $id;
+  public $type;
+  public $name;
+  public $content;
+  public $proxiable;
+  public $proxied;
+  public $ttl;
+  public $locked;
+  public $zone_id;
+  public $zone_name;
+  public $created_on;
+  public $modified_on;
+  public $data;
+}
