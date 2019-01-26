ac44aa909664e47abd8d7d0444eb8e086f647486
Sander Jochems <sanderjochems@hotmail.nl>
2019-01-26 11:37:13 +0100

    Update SettingsUtil.php

--- plib/controllers/classes/SettingsUtil.php
+++ plib/controllers/classes/SettingsUtil.php
@@ -5,7 +5,14 @@ class SettingsUtil
   const CLOUDFLARE_EMAIL = 'cloudflareEmail';
   const CLOUDFLARE_API_KEY = 'cloudflareApiKey';
 
+  const CLOUDFLARE_PROXY = 'cloudflareProxy';
+  const CLOUDFLARE_SYNC_TYPES = 'cloudflareSyncTypes';
+
   public static function getUserKey($key) {
     return 'u'.pm_Session::getClient()->getId().'_'.$key;
+  }
+
+  public static function getDomainKey($key, $site_id) {
+    return 'd'.$site_id.'_'.$key;
   }
 }
