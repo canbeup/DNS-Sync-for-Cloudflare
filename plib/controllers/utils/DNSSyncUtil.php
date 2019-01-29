577a6b279e1cb0cb382cae77c5757f98f9fc412e
Sander0542 <sanderjochems@hotmail.nl>
2019-01-29 11:19:16 +0100

    Update DNSSyncUtil.php

--- plib/controllers/utils/DNSSyncUtil.php
+++ plib/controllers/utils/DNSSyncUtil.php
@@ -1,14 +1,30 @@
 <?php
 
+use Cloudflare\API\Endpoints\DNS;
+use PleskX\Api\Struct\Dns\Info;
+
 require_once 'DNSUtilBase.php';
 
 class DNSSyncUtil extends DNSUtilBase
 {
+  /**
+   * DNSSyncUtil constructor.
+   * 
+   * @param $siteID
+   * @param Cloudflare $cloudflare
+   * @param PleskDNS $pleskDNS
+   * @throws pm_Exception
+   */
   public function __construct($siteID, Cloudflare $cloudflare, PleskDNS $pleskDNS)
   {
     parent::__construct($siteID, $cloudflare, $pleskDNS);
   }
 
+  /**
+   * Sync all the records in Plesk
+   *
+   * @param pm_View_Status $view_Status
+   */
   public function syncAll(pm_View_Status $view_Status) {
     $recordsUpdated = 0;
     $recordsCreated = 0;
@@ -20,43 +36,94 @@ class DNSSyncUtil extends DNSUtilBase
 
       if (DomainSettingsHelper::syncRecordType($pleskRecord->type, $this->siteID)) {
 
-        $cloudflareRecord = $this->getCloudflareRecord($pleskRecord);
-
-        if ($cloudflareRecord !== false) {
-
-          if (!$this->doRecordsMatch($pleskRecord, $cloudflareRecord)) {
-
-            //Update the record in Cloudflare
-            $cloudflareDNS->updateRecordDetails($this->zoneID, $cloudflareRecord->id, array(
-                'type' => $pleskRecord->type,
-                'name' => $pleskRecord->host,
-                'content' => $pleskRecord->value,
-                'proxied' => DomainSettingsHelper::useCloudflareProxy($pleskRecord->siteId)
-            ));
-
-            $recordsUpdated++;
-          }
-
-        } else {
-          $proxied = false;
-          $priority = '';
-
-          if ($pleskRecord->type == 'A' || $pleskRecord->type == 'AAAA' || $pleskRecord->type == 'CNAME') {
-            $proxied = DomainSettingsHelper::useCloudflareProxy($this->siteID);
-          }
-
-          if ($pleskRecord->type == 'MX') {
-            $priority = $pleskRecord->opt;
-          }
-
-          //Create a new record in cloudflare
-          if ($cloudflareDNS->addRecord($this->zoneID, $pleskRecord->type, $pleskRecord->host, $pleskRecord->value, 0, $proxied, $priority) === true) {
-            $recordsCreated++;
-          }
-        }
+        $this->sync($pleskRecord, $cloudflareDNS, $recordsUpdated, $recordsCreated);
 
       }
 
+    }
+
+    $this->addMessage($view_Status, $recordsCreated, $recordsUpdated);
+  }
+
+  /**
+   * Sync a single record
+   *
+   * @param pm_View_Status $view_Status
+   * @param $recordID
+   */
+  public function syncRecord(pm_View_Status $view_Status, $recordID) {
+    $recordsUpdated = 0;
+    $recordsCreated = 0;
+
+    //Get the Cloudflare DNS
+    $cloudflareDNS = $this->cloudflare->getDNS();
+
+    $pleskRecord = $this->pleskDNS->getRecord($recordID);
+
+    if ($pleskRecord !== false && DomainSettingsHelper::syncRecordType($pleskRecord->type, $this->siteID)) {
+
+      $this->sync($pleskRecord, $cloudflareDNS, $recordsUpdated, $recordsCreated);
+
+    }
+
+    $this->addMessage($view_Status, $recordsCreated, $recordsUpdated);
+  }
+
+  /**
+   * @param Info $pleskRecord
+   * @param DNS $cloudflareDNS
+   * @param $recordsUpdated
+   * @param $recordsCreated
+   */
+  private function sync(Info $pleskRecord, DNS $cloudflareDNS, &$recordsUpdated, &$recordsCreated) {
+
+    //Get the cooresponding cloudflare record
+    $cloudflareRecord = $this->getCloudflareRecord($pleskRecord);
+
+    //Check if the record exists
+    if ($cloudflareRecord !== false) {
+      //If so, then check if the records need to be updated
+      if (!$this->doRecordsMatch($pleskRecord, $cloudflareRecord)) {
+        //If so, then update the record in Cloudflare
+        $cloudflareDNS->updateRecordDetails($this->zoneID, $cloudflareRecord->id, array(
+            'type' => $pleskRecord->type,
+            'name' => $pleskRecord->host,
+            'content' => $pleskRecord->value,
+            'proxied' => DomainSettingsHelper::useCloudflareProxy($pleskRecord->siteId)
+        ));
+
+        $recordsUpdated++;
+      }
+
+    } else {
+      //If not, then create a new record
+      $proxied = false;
+      $priority = '';
+
+      if ($pleskRecord->type == 'A' || $pleskRecord->type == 'AAAA' || $pleskRecord->type == 'CNAME') {
+        $proxied = DomainSettingsHelper::useCloudflareProxy($this->siteID);
+      }
+
+      if ($pleskRecord->type == 'MX') {
+        $priority = $pleskRecord->opt;
+      }
+
+      //Create a new record in cloudflare
+      if ($cloudflareDNS->addRecord($this->zoneID, $pleskRecord->type, $pleskRecord->host, $pleskRecord->value, 0, $proxied, $priority) === true) {
+        $recordsCreated++;
+      }
+    }
+  }
+
+  /**
+   * @param pm_View_Status $view_Status
+   * @param $recordsCreated
+   * @param $recordsUpdated
+   */
+  private function addMessage(pm_View_Status $view_Status, $recordsCreated, $recordsUpdated) {
+    if ($recordsUpdated == 0 && $recordsCreated == 0) {
+      $view_Status->addMessage('error', 'No records created of updated.');
+      return;
     }
 
     if ($recordsCreated > 0) {
