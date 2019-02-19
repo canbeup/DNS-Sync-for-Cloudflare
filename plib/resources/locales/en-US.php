fb99ee8d768bc892823d72700964dfa71614f018
Sander Jochems <sanderjochems@hotmail.nl>
2019-02-19 23:36:00 +0100

    Create en-US.php

---
+++ plib/resources/locales/en-US.php
@@ -0,0 +1,40 @@
+<?php
+
+$messages = [
+    'title' => 'Cloudflare DNS Sync',
+    'description' => 'Sync the Plesk DNS to Cloudflare DNS',
+
+    'title.cloudflareSyncFor' => 'Cloudflare DNS Sync for <b>%%domain%%</b>',
+
+    'tab.domains' => 'Domains',
+    'tab.api' => 'API',
+    'tab.dns' => 'DNS',
+    'tab.settings' => 'Settings',
+
+    'form.cloudflareEmail' => 'Cloudflare Email',
+    'form.cloudflareApiKey' => 'Cloudflare API Key',
+
+    'form.trafficThruCloudflare' => 'Traffic Thru Cloudflare',
+    'form.selectRecord' => 'Select the type of records you want to sync',
+
+    'table.domainName' => 'Domain Name',
+    'table.cloudflareZoneID' => 'Cloudflare Zone ID',
+
+    'table.host' => 'Cloudflare Zone ID',
+    'table.recordType' => 'Cloudflare Zone ID',
+    'table.status' => 'Cloudflare Zone ID',
+    'table.cloudflareValue' => 'Cloudflare Value',
+    'table.pleskValue' => 'Plesk Value',
+
+    'button.syncDNS' => 'Sync DNS',
+
+    'text.zoneIdNotFound' => 'Zone ID not found',
+
+    'message.apiSaved' => 'API Settings were successfully saved.',
+    'message.settingsSaved' => 'Domain Settings were successfully saved.',
+    'message.noConnection' => 'Could not connect to Cloudflare.',
+    'message.couldNotSync' => 'Could not sync the Plesk DNS zone to Cloudflare.',
+    'message.noCloudflareZoneFound' => 'Could not find a Cloudflare zone for this domain.',
+    'message.noAccessToDomain' => 'You do not have access to this domain.',
+    'message.noDomainSelected' => 'There was no domain selected.',
+];
