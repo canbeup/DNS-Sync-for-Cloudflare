635970327813b8391e1315a91f6305165584cab5
Sander0542 <sanderjochems@hotmail.nl>
2019-02-20 09:13:56 +0100

    Create Dutch Locale

---
+++ plib/resources/locales/nl-NL.php
@@ -0,0 +1,40 @@
+<?php
+
+$messages = [
+    'title' => 'Cloudflare DNS Sync',
+    'description' => 'Synchroniseer de DNS van Plesk met Cloudflare DNS',
+
+    'title.cloudflareSyncFor' => 'Cloudflare DNS Sync voor <b>%%domain%%</b>',
+
+    'tab.domains' => 'Domeinen',
+    'tab.api' => 'API',
+    'tab.dns' => 'DNS',
+    'tab.settings' => 'Instellingen',
+
+    'form.cloudflareEmail' => 'Cloudflare Email',
+    'form.cloudflareApiKey' => 'Cloudflare API Key',
+
+    'form.trafficThruCloudflare' => 'Verkeer door Cloudflare',
+    'form.selectRecord' => 'Selecteer het type records dat u wilt synchroniseren',
+
+    'table.domainName' => 'Domein Naam',
+    'table.cloudflareZoneID' => 'Cloudflare Zone ID',
+
+    'table.host' => 'Cloudflare Zone ID',
+    'table.recordType' => 'Record Type',
+    'table.status' => 'Status',
+    'table.cloudflareValue' => 'Cloudflare Waarde',
+    'table.pleskValue' => 'Plesk Waarde',
+
+    'button.syncDNS' => 'Synchroniseer DNS',
+
+    'text.zoneIdNotFound' => 'Zone ID niet gevonden',
+
+    'message.apiSaved' => 'API-instellingen zijn succesvol opgeslagen.',
+    'message.settingsSaved' => 'Domeininstellingen zijn succesvol opgeslagen.',
+    'message.noConnection' => 'Kon niet verbinden met Cloudflare.',
+    'message.couldNotSync' => 'De Plesk DNS-zone kon niet worden gesynchroniseerd met Cloudflare.',
+    'message.noCloudflareZoneFound' => 'Kan geen Cloudflare-zone voor dit domein vinden.',
+    'message.noAccessToDomain' => 'U hebt geen toegang tot dit domein.',
+    'message.noDomainSelected' => 'Er was geen domein geselecteerd.',
+];
